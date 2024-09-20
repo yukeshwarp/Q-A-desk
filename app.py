42022ab1f3e379ac86db0af24a954141570f0d5e
yukeshwarp <Yukeshwarp@docu3c.com>
2024-09-20 09:50:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import PyPDF2
 import streamlit as st
 from langchain_community.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
@@ -57,8 +56,8 @@ def main():
             # Extract text from the PDF
             all_text = extract_text_from_pdf(pdf_path)
             text_splitter = RecursiveCharacterTextSplitter(
-                chunk_size=1000,
-                chunk_overlap=200,
+                chunk_size=2000,
+                chunk_overlap=500,
                 length_function=len,
                 separators=['\n', '\n\n', ' ', '']
             )
@@ -79,11 +78,18 @@ def main():
  
         # Azure OpenAI and prompt settings
         llm = AzureChatOpenAI(deployment_name="gpt-4o-mini")
-        prompt_template = """Answer the question as precise as possible using the provided context. If the answer is
-                            not contained in the context, say "answer not available in context" \n\n
-                            Context: \n {context}?\n
-                            Question: \n {question} \n
-                            Answer:"""
+        prompt_template = """Using the provided context, answer the question in a detailed and comprehensive manner. 
+                             Make sure your response fully addresses the question and provides as much relevant information as possible. 
+                             If the answer is not available in the context, respond with 'Answer not available in context.' 
+                             Do not shorten or omit any important details from the answer.
+
+                             Context:
+                             {context}
+
+                             Question:
+                             {question}
+
+                             Answer:"""
         prompt = PromptTemplate.from_template(template=prompt_template)
  
         # Q&A Interaction
